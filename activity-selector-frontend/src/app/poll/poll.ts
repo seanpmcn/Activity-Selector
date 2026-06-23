aed420184bba0aa2f2610ed958327ebfeeb2bde3
seanpmcn <46979553+seanpmcn@users.noreply.github.com>
2026-06-23 16:58:35 -0400

    Add poll component

---
+++ activity-selector-frontend/src/app/poll/poll.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-poll',
+  imports: [],
+  templateUrl: './poll.html',
+  styleUrl: './poll.css',
+})
+export class Poll {}

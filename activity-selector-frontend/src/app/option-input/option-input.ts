4b4d10b6c361684efb97641a1f22715199ec0de9
seanpmcn <seanmcnally@outlook.com>
2026-06-25 11:25:47 -0400

    Add option input component

---
+++ activity-selector-frontend/src/app/option-input/option-input.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-option-input',
+  imports: [],
+  templateUrl: './option-input.html',
+  styleUrl: './option-input.css',
+})
+export class OptionInput {}
